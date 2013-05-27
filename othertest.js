0088b2fa8345d5222dc6f316c063c03af7194d0d
root <root@raspberrypi.(none)>
2013-05-27 19:00:20 +0000

    Added jquery and removed reset.css

--- othertest.js
+++ othertest.js
@@ -1,15 +1,20 @@
 var gpio = require('pi-gpio');
 
 var id = 12;
-gpio.open(id, 'output', function (err) {
+gpio.close(id, function () {
+
+  gpio.open(id, 'output', function (err) {
   
-  console.log('Hopefully turning the light on.');
-  gpio.write(id, 1, function () {
+    console.log('Hopefully turning the light on.');
+    gpio.write(id, 1, function () {
+  
+      console.log('Light is on.');
+    });
+    setTimeout(function(){
+      gpio.write(id, 0, function () {  
+  	console.log('Light is off.');
+      });
+    }, 5000);
 
-    console.log('Light is on.. maybe?');
-    setTimeout(function () {
-      gpio.close(id);
-    },200000);
   });
-
 });
